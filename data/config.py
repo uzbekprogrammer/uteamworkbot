57dc1e2b744a42488f002dce317491a2e1c636c0
uzbekprogrammer <mahmudovabdurahim04@gmail.com>
2022-02-23 11:55:21 +0300

    add Procfile

--- data/config.py
+++ data/config.py
@@ -1,11 +1,19 @@
-from environs import Env
+# from environs import Env
+#
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+#
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+# CHANNELS = env.str("CHANNEL")
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+import os
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
-CHANNELS = env.str("CHANNEL")
+BOT_TOKEN = str(os.environs.str("BOT_TOKEN"))  # Bot toekn
+ADMINS = list(os.environs.list("ADMINS"))  # adminlar ro'yxati
+IP = str(os.environs.str("ip"))  # Xosting ip manzili
+CHANNELS = str(os.environs.str("CHANNEL"))
+PROVIDER_TOKEN = str(os.environs.str("PROVIDER_TOKEN"))
